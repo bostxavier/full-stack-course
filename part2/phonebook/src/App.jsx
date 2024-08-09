5ed97b746d2a3ae70c5f0b5c9eca669e947256cf
bostxavier <xavierbost@orange.fr>
2024-08-09 16:17:10 +0200

    chore: complete exercise 2.10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
@@ -46,26 +50,16 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-          filter shown with: <input value={filter} onChange={handleFilterChange} />
-        </div>
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
       <h2>Add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
-      {filteredPersons.map(person => 
-        <div key={person.name}>
-          {person.name} {person.number}
-        </div>)}
+      <Persons filteredPersons={filteredPersons} />
     </div>
   )
 }
---
+++ part2/phonebook/src/components/Persons.jsx
@@ -0,0 +1,12 @@
+import Person from './Person'
+
+const Persons = ({filteredPersons}) => {
+    return (
+        <>
+            {filteredPersons.map(person => 
+                <Person key={person.name} person={person} />)}
+        </>
+    )
+}
+
+export default Persons
