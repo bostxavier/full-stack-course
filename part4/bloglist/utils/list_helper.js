67fc53797ded6e9ce45481f709f40999ec0d400d
bostxavier <xavierbost@orange.fr>
2024-08-21 14:56:18 +0200

    feat: complete exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce(
+    (acc, v) => acc + v.likes,
+    0
+  )
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
