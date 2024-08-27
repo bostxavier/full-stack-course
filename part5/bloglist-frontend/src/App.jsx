5160a9bc7a2f31c95c529946a1b9bab306e4a6e3
bostxavier <xavierbost@orange.fr>
2024-08-27 16:31:02 +0200

    feat: complete excercise 5.7

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -118,7 +118,7 @@ const App = () => {
         <BlogForm createBlog={createBlog} />
       </Togglable>
       {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+        <Blog key={blog.id} blog={blog} user={user} />
       )}
     </>
   )
--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,41 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React from 'react'
+import { useState } from 'react'
+
+const Blog = ({ blog, user }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const toggleShowDetails = () => setShowDetails(!showDetails)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} {' '}
+        <button onClick={toggleShowDetails}>{showDetails ? 'hide' : 'view'}</button>
+      </div>
+      {showDetails && (
+        <>
+          <div>
+            {blog.url}
+          </div>
+          <div>
+            likes {blog.likes}
+            <button>like</button>
+          </div>
+          <div>
+            {user.name}
+          </div>
+        </>
+      )}
+    </div>
+  )
+}
 
 export default Blog
