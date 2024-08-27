5da60b93832215c4223ce99a20e6369ae103d68c
bostxavier <xavierbost@orange.fr>
2024-08-27 17:48:31 +0200

    feat: complete exercise 5.10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -125,9 +125,11 @@ const App = () => {
       <Togglable buttonLabel='new note'>
         <BlogForm createBlog={createBlog} />
       </Togglable>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} user={user} updateBlog={updateBlog} />
-      )}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog =>
+          <Blog key={blog.id} blog={blog} user={user} updateBlog={updateBlog} />)
+      }
     </>
   )
 }
