5e2161d44eb8db4255dc91aaf43aee698f146630
bostxavier <xavierbost@orange.fr>
2024-08-28 11:58:19 +0200

    feat: complete exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,23 +1,49 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-let container
-
 describe('<Blog />', () => {
-  test('renders blog\'s title and author by default, but not url and likes', () => {
-    const blog = {
-      title: 'React patterns',
-      author: 'Michael Chan',
-      url: 'https://reactpatterns.com/',
-      likes: 7,
-    }
+  let container
 
-    container = render(<Blog blog={blog} />).container
+  const blog = {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    user: {
+      username: 'hellas',
+      name: 'Arto Hellas',
+      id: '66ce02d9789f170a563292fa'
+    }
+  }
+
+  const user = {
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Inhib3N0IiwiaWQiOiI2NmNlMDJjNDc4OWYxNzBhNTYzMjkyZjgiLCJpYXQiOjE3MjQ4MjkxMDd9.PCpgTvsi69LVTGznhWvxR0OjX60jrojvxPX_2G2MI5g',
+    username: 'xbost',
+    name: 'Xavier'
+  }
+
+  beforeEach(() => {
+    container = render(<Blog blog={blog} user={user} />).container
+  })
+
+  test('renders blog\'s title and author by default, but not url and likes', () => {
     const content = container.querySelector('.blogContent')
 
     expect(content).toHaveTextContent(blog.author)
     expect(content).toHaveTextContent(blog.title)
     expect(content).not.toHaveTextContent(blog.url)
     expect(content).not.toHaveTextContent('likes')
+  })
+
+  test('renders blog\'s url and likes once `view` button is clicked', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const content = container.querySelector('.blogContent')
+
+    expect(content).toHaveTextContent(blog.url)
+    expect(content).toHaveTextContent('likes')
   })
 })
