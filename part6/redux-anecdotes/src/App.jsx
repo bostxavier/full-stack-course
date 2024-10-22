5643e447d4d681d48b451219757617f741652ebb
Xavier Bost <xavierbost@orange.fr>
2024-10-22 10:09:37 +0200

    feat: complete exercise 6.9

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,10 +1,12 @@
-import AnecdoteList from './AnecdoteList'
-import AnecdoteForm from './AnecdoteForm'
+import Filter from './components/Filter'
+import AnecdoteList from './components/AnecdoteList'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
   return (
     <>
       <h2>Anecdotes</h2>
+      <Filter />
       <AnecdoteList />
       <AnecdoteForm />
     </>
--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,9 +1,11 @@
 import { useSelector, useDispatch } from 'react-redux'
 
-import { incrementVote } from './reducers/anecdoteReducer'
+import { incrementVote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-    const anecdotes = useSelector(state => state)
+    const anecdotes = useSelector(state => {
+        return state.anecdotes.filter(a => a.content.includes(state.filter))
+    })
     const dispatch = useDispatch()
 
     const vote = (id) => {
