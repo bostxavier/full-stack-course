94c561c6ce64c2ca00d0b8ed32e35b677a6373db
Xavier Bost <xavierbost@orange.fr>
2024-10-22 12:05:07 +0200

    feat: complete exercise 6.10

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,17 +1,16 @@
-const filterReducer = (state = '', action) => {
-    switch (action.type) {
-      case 'SET_FILTER':
-        return action.payload
-      default:
-        return state
-    }
-  }
+import { createSlice } from '@reduxjs/toolkit'
 
-  export const filterChange = filter => {
-    return {
-        type: 'SET_FILTER',
-        payload: filter
-    }
-  }
+const initialState = ''
 
-  export default filterReducer
+const anecdoteSlice = createSlice({
+    name: 'filter',
+    initialState,
+    reducers: {
+        filterChange(state, action) {
+            return action.payload
+        }  
+    }
+})
+
+export const { filterChange } = anecdoteSlice.actions
+export default anecdoteSlice.reducer
