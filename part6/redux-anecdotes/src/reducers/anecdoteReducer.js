a93f63a6762e2d3dafe8de9aa8dfc926d67e499f
Xavier Bost <xavierbost@orange.fr>
2024-10-23 11:02:37 +0200

    feat: complete exercise 6.16

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,5 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+import anecdoteService from '../services/anecdotes'
+
 const initialState = []
 
 const anecdoteSlice = createSlice({
@@ -29,4 +31,12 @@ const anecdoteSlice = createSlice({
 })
 
 export const { incrementVote, createAnecdote, setAnecdotes } = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
+
 export default anecdoteSlice.reducer
