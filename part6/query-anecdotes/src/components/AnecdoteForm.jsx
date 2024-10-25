c4392dd9c25928b49f28318eedcb7d2c7e9577dc
Xavier Bost <xavierbost@orange.fr>
2024-10-25 11:59:36 +0200

    feat: complete exercise 6.21

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,17 +1,29 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query' 
+
+import { createAnecdote } from '../requests'
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    },
+  })
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
+    newAnecdoteMutation.mutate({ content, votes: 0 })
 }
 
   return (
     <div>
       <h3>create new</h3>
       <form onSubmit={onCreate}>
-        <input name='anecdote' />
+        <input name='anecdote' defaultValue="Never attribute to malice that which can be adequately explained by stupidity."/>
         <button type="submit">create</button>
       </form>
     </div>
